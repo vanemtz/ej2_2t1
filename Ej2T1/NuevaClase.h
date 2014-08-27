bd19289ad24b72a93b55ef458fc136881b3cfb8b
admin <admin@MacBook-Pro-de-admin-2.local>
2014-08-27 10:46:35 -0500

    se agrega nueva clase

---
+++ Ej2T1/NuevaClase.h
@@ -0,0 +1,13 @@
+//
+//  NuevaClase.h
+//  Ej2T1
+//
+//  Created by admin on 27/08/14.
+//  Copyright (c) 2014 scoutech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NuevaClase : NSObject
+
+@end
---
+++ Ej2T1/NuevaClase.m
@@ -0,0 +1,13 @@
+//
+//  NuevaClase.m
+//  Ej2T1
+//
+//  Created by admin on 27/08/14.
+//  Copyright (c) 2014 scoutech. All rights reserved.
+//
+
+#import "NuevaClase.h"
+
+@implementation NuevaClase
+
+@end
